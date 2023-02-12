4e334c865fffce3b1f42b3c81d5994664ad23cb2
DenisseDiaz <96696742+DenisseDiaz@users.noreply.github.com>
2023-02-12 12:11:23 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBW3Fthvj5l1JyzQxzkREIkIpHcuT-kR4I",
+    authDomain: "ideart-e2da6.firebaseapp.com",
+    projectId: "ideart-e2da6",
+    storageBucket: "ideart-e2da6.appspot.com",
+    messagingSenderId: "451091721914",
+    appId: "1:451091721914:web:55f0216ad5c448db155c83"
+  };
